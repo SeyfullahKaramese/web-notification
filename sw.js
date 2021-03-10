c096b343c27930d4305fd7602355e98bbb71e439
Seyfullah KARAMEŞE <seyfullahkaramese@gmail.com>
2021-03-10 19:58:17 +0300

    update

--- sw.js
+++ sw.js
@@ -1,12 +1,18 @@
 self.addEventListener("push", (e) => {
     const config = {
-        body:e.data.text() || "Yeni Mesaj İçerii Burada olacak",
+        body: e.data.text() || "Yeni Mesaj İçerii Burada olacak",
         data: {
             dateOfArrival: Date.now(),
             primaryKey: "3"
         },
-        icon:"images/logo.png",
-        vibrate:[100,50,100]
+        icon: "images/logo.png",
+        vibrate: [100, 50, 100],
+        action: [{
+            action: "explore",title: "Gözat"
+        },
+        {
+            action: "close", title: "Bildirimleri Kapat"
+        }]
     }
     e.waitUntil(self.registration.showNotification("Yeni Makale Eklendi", config))
 })
